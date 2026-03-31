226eb695527e57d6001bfda66a082e1f01669d37
Ryad Lachemi <238238672+ryadl14@users.noreply.github.com>
2026-03-31 18:15:49 +0100

    Added comments to preprocessing script.

--- src/scrna_pipeline/preprocessing.py
+++ src/scrna_pipeline/preprocessing.py
@@ -1,21 +1,21 @@
 import scanpy as sc
 
-def normalise_data (adata, target_sum = 1e4):
+def normalise_data (adata, target_sum = 1e4): # Cell counts are scaled to add up to 10,000
     sc.pp.normalize_total(adata, target_sum=target_sum)
     print ("Normalisation complete!")
     return adata
 
-def log_transform_data (adata):
+def log_transform_data (adata): # Log transform all values.
     sc.pp.log1p(adata)
     print ("Log transformation complete!")
     return adata
 
-def hvg_selection (adata, n_top_genes = 2000):
-    sc.pp.highly_variable_genes(adata, n_top_genes=n_top_genes, subset=False)
+def hvg_selection (adata, n_top_genes = 2000): # Selects the top 2000 highly variable genes.
+    sc.pp.highly_variable_genes(adata, n_top_genes=n_top_genes, subset=False) # Does not subset them, just flags the top 2000 in a boolean "highly_variable" row.
     print (f"{n_top_genes} highly variable genes selected!")
     return adata
 
-def run_preprocessing(adata):
+def run_preprocessing(adata): # Runs all the functions sequentially. 
     print ("Starting normalisation...")
     adata = normalise_data(adata)
     print ("Starting log transformation...")
@@ -24,7 +24,7 @@ def run_preprocessing(adata):
     adata = hvg_selection(adata)
     return adata
 
-if __name__ == "__main__":
+if __name__ == "__main__": # Runs the whole pipeline up until this point.
     from .data_loading import load_pbmc
     from .qc import run_qc
     adata = load_pbmc() 
